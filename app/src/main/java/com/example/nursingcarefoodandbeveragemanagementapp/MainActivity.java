8333b1cdabf21234ca0bb540aa5b595c7fa5fc88
tuan <tranthanhtuan199981@gmail.com>
2022-10-31 16:29:43 +0900

    thu nghiem

--- app/src/main/java/com/example/nursingcarefoodandbeveragemanagementapp/MainActivity.java
+++ app/src/main/java/com/example/nursingcarefoodandbeveragemanagementapp/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.nursingcarefoodandbeveragemanagementapp;
 
+import static androidx.constraintlayout.helper.widget.MotionEffect.TAG;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d(TAG, "onCreate: tuan");
     }
 }
